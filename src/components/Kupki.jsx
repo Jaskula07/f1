982388380d1fe1cb6030e15008714d3dedee3512
Jaskula07 <jaskula07@gmail.com>
2025-04-20 20:20:33 +0200

    Aktualisieren von Kupki.jsx

--- src/components/Kupki.jsx
+++ src/components/Kupki.jsx
@@ -114,7 +114,7 @@ function Kupki({ onBack }) {
       {/* Lista utworzonych kupek */}
       <div className="list-kupki">
         <h3>Twoje kupki</h3><br>
-<h1>Odwzoruj te kupki w Realu! 
+<h1>Odwzoruj te kupki w Realu!</h1>
         {kupki.length === 0 ? (
           <p>Nie utworzono jeszcze żadnej kupki.</p>
         ) : (
